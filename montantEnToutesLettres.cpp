2a79c2b807a189a95eb689e37af0ffcb18e7fe8a
Arthur2479 <arthurjacobs1004@gmail.com>
2023-10-06 20:18:03 +0200

    Add numberToText, which convert any number between 0 and 999'999'999 to text + cleanup and fixes

--- montantEnToutesLettres.cpp
+++ montantEnToutesLettres.cpp
@@ -19,6 +19,10 @@ string convertTens(int tens);
 
 string tensToText(int smallInt);
 
+string hundredsToText(int hundred, bool mille_after);  //# FIXME : better to set default val here or en bas ?
+
+string numberToText(long int number);
+
 
 string convertUnit(int unit) {
     switch (unit) {
@@ -38,7 +42,7 @@ string convertUnit(int unit) {
 
 string convertSpecial(int special) {
     switch (special) {
-        case 10: return "dix"; // FIXME : dix already in convertTen, remove one
+        case 10: return "dix";
         case 11:return "onze";
         case 12:return "douze";
         case 13:return "treize";
@@ -51,7 +55,6 @@ string convertSpecial(int special) {
 
 string convertTens(int tens) {
     switch (tens) {
-        case 1:return "dix";
         case 2:return "vingt";
         case 3:return "trente";
         case 4:return "quarante";
@@ -83,7 +86,7 @@ string tensToText(int smallInt) {
     }
 }
 
-string hundredsToText(int hundred) {
+string hundredsToText(int hundred, bool mille_after = false) {
 //    0-999
     int quotient = hundred / 100;
     int rest = hundred % 100;
@@ -92,7 +95,7 @@ string hundredsToText(int hundred) {
         return tensToText(hundred);
     }
 
-    string cent = (quotient > 1 && rest == 0 ? "cents" : "cent");
+    string cent = (quotient > 1 && rest == 0 && !mille_after ? "cents" : "cent");
     string tens = (rest == 0 ? "" : "-" + tensToText(rest));
     if (quotient == 1) {
         return cent + tens;
@@ -101,24 +104,59 @@ string hundredsToText(int hundred) {
     }
 }
 
+const int one_billion = 1e9,
+        one_million = 1e6,
+        one_thousand = 1e3;
+
+string numberToText(long int number) {
+    string number_text;
+
+    int order = number / one_billion;
+    long int rest = number - order * one_billion;
+    if (order >= 1) {
+        number_text += hundredsToText(order) + "-milliard" + (order > 1 ? "s" : "") + (rest != 0 ? "-" : "");
+    }
+
+    order = rest / one_million;
+    rest -= order * one_million;
+    if (order >= 1) {
+        number_text += hundredsToText(order) + "-million" + (order > 1 ? "s" : "") + (rest != 0 ? "-" : "");
+    }
+
+    order = rest / one_thousand;
+    rest -= order * one_thousand;
+    if (order == 1) {
+        number_text = number_text + "mille" + (rest != 0 ? "-" : "");
+    } else if (order >= 1) {
+        number_text += hundredsToText(order, true) + "-mille" + (rest != 0 ? "-" : "");
+    }
+
+    if (rest >= 1) {
+        number_text += hundredsToText(rest);
+    }
+
+    return number_text;
+}
+
 //--ROUNDING FUNCIONS ------------------------------------------------
-long double roundFractional(long double montant){
+long double roundFractional(long double montant) {
     long double x = round(montant * 100) / 100;
     x = round(montant * 100) / 100;
-    return x ;
+    return x;
 }
 
 //---------------------------------------------------------------------
 
+// TODO Est-ce qu'on passerait pas decimalPart et fractionalPart par référence pouréviter d'appeler 2* separator ?
 //--SEPARATION OF THE DECIMAL AND FRACTIONAL --------------------------
-long int separator(long double montant, bool decimalOrFractional){
+long int separator(long double montant, bool decimalOrFractional) {
     // Fractional == False,  Decimal == True
     double decimalPart;
     double fractionalPart = modf(montant, &decimalPart);
 
-    if(decimalOrFractional){
+    if (decimalOrFractional) {
         return decimalPart;
-    }else {
+    } else {
         fractionalPart = roundFractional(fractionalPart);//added for fix rounding error of c++
         fractionalPart *= 100;
         return fractionalPart;
@@ -131,5 +169,6 @@ long int separator(long double montant, bool decimalOrFractional){
 
 
 string montantEnToutesLettres(long double montant) {
-  return "zero franc"s;
+    return numberToText(montant);
+    return "zero franc"s;
 }
